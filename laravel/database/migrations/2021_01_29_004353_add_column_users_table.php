3bc6504cd8bfe167e46d47c609610ef84ed0f99d
[fujii_yusuke] <[fujii]@[barnet.co.jp]>
2021-01-29 10:24:34 +0900

    マイグレーション完了

---
+++ laravel/database/migrations/2021_01_29_004353_add_column_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('code',3)->nullable();
+            $table->string('role_name',30);
+            // 論理削除フラグ
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
