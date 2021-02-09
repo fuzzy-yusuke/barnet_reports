ac877bf085af47fef49d495121718ba53956b0e1
[fujii_yusuke] <[fujii]@[barnet.co.jp]>
2021-02-09 13:41:33 +0900

    コメント位置修正

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -23,48 +23,49 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/', 'TopController@index');
 
     // 一般ユーザー
-    Route::get('/user/top', 'UserController@top')->name('user.top'); //一般ユーザーTOP画面
-    Route::get('/user/account/index', 'UserController@accountIndex')->name('user.accountIndex'); //アカウント情報画面
-    Route::get('/user/enquete/index', 'QuestionController@answerIndex')->name('user.answerIndex'); //アンケート回答画面
-    Route::post('/user/enquete/confirm', 'QuestionController@answerConfirm')->name('user.answerConfirm'); //アンケート回答確認画面
-    Route::get('/user/enquete/complete', 'QuestionController@answerComplete')->name('user.answerComplete'); //アンケート回答完了画面
+    //一般ユーザーTOP画面
+    Route::get('/user/top', 'UserController@top')->name('user.top');
+
+    //アカウント情報画面
+    Route::get('/user/account/index', 'UserController@accountIndex')->name('user.accountIndex');
+
+    //アンケート回答画面
+    Route::get('/user/enquete/index', 'QuestionController@answerIndex')->name('user.answerIndex');
+    //アンケート回答確認画面
+    Route::post('/user/enquete/confirm', 'QuestionController@answerConfirm')->name('user.answerConfirm');
+    //アンケート回答完了画面
+    Route::get('/user/enquete/complete', 'QuestionController@answerComplete')->name('user.answerComplete');
 
 
     // 管理者
-    Route::get('/admin/top', 'AdminController@top')->name('admin.top'); //管理者TOP画面
-    Route::get('/admin/account/list', 'AdminController@accountList')->name('admin.accountList'); //アカウント一覧
-    /*Route::get('/admin/account/create', 'AdminController@accountCreate'); //アカウント作成
-    Route::post('/admin/account/create', 'AdminController@accountStore'); //アカウント作成処理 */
+    //管理者TOP画面
+    Route::get('/admin/top', 'AdminController@top')->name('admin.top');
 
-    /*registerのルーティング
-    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-    Route::post('login', 'Auth\LoginController@login');
-    Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-
-    Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-    Route::post('register', 'Auth\RegisterController@register');
-
-
-    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-    Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
-    */
-
-    //Route::get('/register', 'Auth\RegisterController@selectable_roles');
+    //アカウント一覧
+    Route::get('/admin/account/list', 'AdminController@accountList')->name('admin.accountList');
+    //アカウント新規作成
     Route::get('/register', 'UserController@registUser');
-    Route::get('/admin/account/edit/{id}', 'AdminController@accountEdit')->name('admin.accountEdit'); //アカウント編集
-    Route::post('/admin/account/edit/{id}', 'AdminController@accountUpdate')->name('admin.accountUpdate'); //アカウント編集
+    //アカウント編集
+    Route::get('/admin/account/edit/{id}', 'AdminController@accountEdit')->name('admin.accountEdit');
+    Route::post('/admin/account/edit/{id}', 'AdminController@accountUpdate')->name('admin.accountUpdate');
+    //アカウント作成完了
     Route::get('/admin/account/complete', 'AdminController@accountComplete')->name('admin.accountComplete');
-    Route::get('/admin/enquete/list', 'QuestionController@questionList')->name('admin.questionList'); //アンケート作成一覧
-    Route::get('/admin/enquete/create', 'QuestionController@questionCreate')->name('admin.questionCreate'); //アンケート作成画面
-    Route::post('/admin/enquete/confirm', 'QuestionController@questionConfirm')->name('admin.questionConfirm'); //アンケート作成確認画面
-    Route::post('/admin/enquete', 'QuestionController@questionStore')->name('admin.questionStore'); //アンケート作成(保存)
-    Route::get('/admin/enquete/complete', 'QuestionController@questionComplete')->name('admin.questionComplete'); //アンケート作成完了画面
-    Route::get('/admin/enquete/read', 'QuestionController@questionRead')->name('admin.questionRead'); //アンケート作成閲覧画面
-    Route::get('/admin/enquete/edit/{id}', 'QuestionController@questionEdit')->name('admin.questionEdit'); //アンケート編集画面
-    Route::post('/admin/enquete/edit/{id}', 'QuestionController@questionUpdate')->name('admin.questionUpdate'); //アンケート編集
-    Route::get('/admin/result/list', 'QuestionController@resultList')->name('admin.resultList'); //アンケート作成閲覧画面
+
+    //アンケート作成一覧
+    Route::get('/admin/enquete/list', 'QuestionController@questionList')->name('admin.questionList');
+    //アンケート新規作成
+    Route::get('/admin/enquete/create', 'QuestionController@questionCreate')->name('admin.questionCreate');
+    //アンケート作成確認
+    Route::post('/admin/enquete/confirm', 'QuestionController@questionConfirm')->name('admin.questionConfirm');
+    Route::post('/admin/enquete', 'QuestionController@questionStore')->name('admin.questionStore');
+    //アンケート作成完了画面
+    Route::get('/admin/enquete/complete', 'QuestionController@questionComplete')->name('admin.questionComplete');
+    //アンケート作成閲覧
+    Route::get('/admin/enquete/read', 'QuestionController@questionRead')->name('admin.questionRead');
+    //アンケート編集
+    Route::get('/admin/enquete/edit/{id}', 'QuestionController@questionEdit')->name('admin.questionEdit');
+    Route::post('/admin/enquete/edit/{id}', 'QuestionController@questionUpdate')->name('admin.questionUpdate');
+    Route::get('/admin/result/list', 'QuestionController@resultList')->name('admin.resultList'); //アンケート作成閲覧
 
 
 
