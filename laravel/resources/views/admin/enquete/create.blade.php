ad6dfdc443cd8017bc94a204d1c2cfed6ba93a55
[fujii_yusuke] <[fujii]@[barnet.co.jp]>
2021-02-11 23:42:30 +0900

    Formファサードからhtmlタグに戻した

--- laravel/resources/views/admin/enquete/create.blade.php
+++ laravel/resources/views/admin/enquete/create.blade.php
@@ -3,12 +3,12 @@
 
     <script src="{{ asset('js/select.js') }}" defer></script>
     <div class="container">
-        {{ Form::open(['route' => 'admin.questionConfirm']) }}
+        <form method="POST" action="{{ route('admin.questionConfirm') }}">
         <div class="col-sm-8 col-sm-offset-2">
             <div class="card" style="width: 150%;">
                 <div class="card-header">アンケート新規作成</div>
                 <div class="card-body">
-                    <h1>{{ Form::text('code', null, ['placeholder' => 'アンケート名']) }}</h1>
+                    <h1><input class="form-control" type="text" id="code" placeholder="アンケート名" ])></h1>
                     <table class="table table-striped">
                         <tr>
                             <th scope="col">No.</th>
@@ -17,29 +17,25 @@
                             <th scope="col">必須</th>
                             <th scope="col">回答選択肢</th>
                         </tr>
-                        {{ Form::token() }}
+                        @csrf
                         <div class="form-group">
                             @for ($i = 1; $i <= 3; $i++)
                                 <tr>
                                     <td>{{ $i }}</td>
-                                    <td>{{ Form::text('content[]', null, ['row' => 50, 'placeholder' => '調子はいかがですか']) }}
+                                    <td><input class="form-control" type="text" id="content[]" name="content[]" row=50 placeholder="調子はいかがですか">
                                     </td>
                                     <td>
-                                        {{--@php
-                                            $formtypes=App\FormType::selectlist();
-                                        @endphp--}}
-                                        <!-- onchange="changefujii();" -->
-                                        <select class="form-control" id="formtypes{{$i}}" name="formtypes[]">
+                                        <select class="form-control" id="formtypes{{ $i }}" name="formtypes[]">
                                             <option value="">選択</option>
                                             @foreach ($formtypes as $formtype)
-                                            <option value={{$formtype->id}}>{{ $formtype->name }}</option>
+                                                <option value={{ $formtype->id }}>{{ $formtype->name }}</option>
                                             @endforeach
                                         </select>
                                         @php
-                                            $formname=$formtype->name;
+                                            $formname = $formtype->name;
                                         @endphp
 
-                                        {{--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js">
+                                        {{-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js">
 
                                             function change($formname){
                                                 var element;
@@ -57,26 +53,26 @@
 
                                             }
                                             console.log(onchange);
-                                        </script>--}}
+                                        </script> --}}
                                     </td>
                                     <td>
-                                        {{ Form::checkbox('must[]', '必須', false, ['id' => 'check-id', 'class' => 'form-check-input']) }}
-                                        {{ Form::label('must', '必須', ['class' => 'form-check-label']) }}
+                                        <input type="checkbox" name="must[]" value="必須" id="check-id" class="form-check-input">
+                                        <label for="must" class="form-check-label">必須</label>
                                     </td>
                                     <td>
                                         <div>
-                                            <input class="form-control" id="item_content1" placeholder="回答欄1" name="item_content1" type="text" disabled="disabled">
+                                            <input class="form-control" id="item_content1" placeholder="回答欄1"name="item_content1" type="text" disabled="disabled">
                                         </div>
                                         <div>
-                                            {{ Form::text('item_content2', null, ['placeholder' => '回答欄2', 'disabled' => 'disabled']) }}
+                                            <input class="form-control" id="item_content2" placeholder="回答欄2"name="item_content2" type="text" disabled="disabled">
                                         </div>
                                         <div>
-                                            {{ Form::text('item_content3', null, ['placeholder' => '回答欄3', 'disabled' => 'disabled']) }}
+                                            <input class="form-control" id="item_content3" placeholder="回答欄3"name="item_content3" type="text" disabled="disabled">
                                         </div>
                                         <div>
-                                            {{ Form::text('item_content4', null, ['placeholder' => '回答欄4', 'disabled' => 'disabled']) }}
+                                            <input class="form-control" id="item_content4" placeholder="回答欄4"name="item_content4" type="text" disabled="disabled">
                                         </div>
-                                        {{ Form::text('item_content5', null, ['placeholder' => '回答欄5', 'disabled' => 'disabled']) }}
+                                        <input class="form-control" id="item_content5" placeholder="回答欄5"name="item_content5" type="text" disabled="disabled">
                                     </td>
                                 </tr>
                             @endfor
@@ -84,14 +80,13 @@
                 </div>
 
                 <div>
-                    {{ Form::label('content', '自由記入欄のタイトル：') }}
-                    {{ Form::text('content[]', null, ['row' => 50]) }}
+                    <label for="content">自由記入欄のタイトル：</label>
+                    <input type="text" name="content[]" row=50>
                 </div>
             </div>
-            {{ Form::button('作成する', ['type' => 'submit', 'class' => 'btn btn-success']) }}
-            {{ Form::button('下書き', ['type' => 'button', 'class' => 'btn btn-info']) }}
+            <input value="作成する" type="submit" class="btn btn-success">
+            <input value="下書き" class="btn btn-info w-auto mt-2">
             <a class="btn btn-secondary" href="{{ route('admin.questionList') }}">戻る</a>
-            {{ Form::close() }}
             </form>
         </div>
     @endsection
