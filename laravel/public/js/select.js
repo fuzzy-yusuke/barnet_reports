4aad185e6ef3ab091a26c2f10240d1aeaeeebfd1
[fujii_yusuke] <[fujii]@[barnet.co.jp]>
2021-02-17 15:12:24 +0900

    テキストボックス以外の回答形式を選択した時、回答選択肢の入力が可能になる

--- laravel/public/js/select.js
+++ laravel/public/js/select.js
@@ -1,7 +1,62 @@
-$(function(){
-    //アンケート新規作成で「テキストボックス」を選択した時、回答選択肢の入力が出来なくなるようにする
-    $('#formtypes1').change(function(){
-        let result=$('#formtypes1').val();
+/*$(function () {
+    for(var i=1;i<3;i++){
+        $('#formtypes[i]').on('change',function(){
+            let result=$('#formtypes[i]').val;
+            if(result=='1'){
+                $('.item_content[i])').attr('disabled',true);
+            }else{
+                $('.item_content[i]').attr('disabled',false);
+            }
+        })
+    }
+    });*/
+
+$(function () {
+
+    $('#formtypes1').on('change', function () {
+        //一番上以外をどうやって取得する？
+        let result = $('#formtypes1').val();
+        var _class = $('[id^=item_content]').attr('class');
+        if (result == '1') {
+            $('.item_content1').attr('disabled', true);
+            console.log(_class);
+        }else{
+            $('.item_content1').attr('disabled', false);
+        }
+    });
+    $('#formtypes2').on('change', function () {
+        //一番上以外をどうやって取得する？
+        let result = $('#formtypes2').val();
+        var _class = $('[id^=item_content]').attr('class');
+        if (result == '1') {
+            $('.item_content2').attr('disabled', true);
+            console.log(_class);
+        }else{
+            $('.item_content2').attr('disabled', false);
+        }
+    });
+    $('#formtypes3').on('change', function () {
+        //一番上以外をどうやって取得する？
+        let result = $('#formtypes3').val();
+        var _class = $('[id^=item_content]').attr('class');
+        if (result == '1') {
+            $('.item_content3').attr('disabled', true);
+            console.log(_class);
+        }else{
+            $('.item_content3').attr('disabled', false);
+        }
+    });
+});
+
+
+
+    // $('input[type=2]').change(function(){
+    //     let hoge=$('.hoge1').attr('class');
+    //     console.log(hoge);
+    // });
+    /*//アンケート新規作成で「テキストボックス」を選択した時、回答選択肢の入力が出来なくなるようにする
+    $('select').change(function(){
+        let result=$('[id^=formtypes]').val();
         if(result =="1"){
             $('#item_content1').prop('disabled',true);
             $('#item_content2').prop('disabled',true);
@@ -15,5 +70,5 @@ $(function(){
             $('#item_content4').prop('disabled',false);
             $('#item_content5').prop('disabled',false);
         }
-    })
-})
+    });*/
+
--- laravel/resources/views/admin/enquete/create.blade.php
+++ laravel/resources/views/admin/enquete/create.blade.php
@@ -18,14 +18,14 @@
                             <th scope="col">回答選択肢</th>
                         </tr>
                         @csrf
-                        <div class="form-group">
+                        <div class="form-group" id="table">
                             @for ($i = 1; $i <= 3; $i++)
                                 <tr>
                                     <td>{{ $i }}</td>
                                     <td><input class="form-control" type="text" id="content[]" name="content[]" row=50 placeholder="調子はいかがですか">
                                     </td>
                                     <td>
-                                        <select class="form-control" id="formtypes{{ $i }}" name="formtypes[]">
+                                        <select class="form-control" id="formtypes{{ $i }}" name="formtypes{{ $i }}">
                                             <option value="">選択</option>
                                             @foreach ($formtypes as $formtype)
                                                 <option value={{ $formtype->id }}>{{ $formtype->name }}</option>
@@ -34,26 +34,6 @@
                                         @php
                                             $formname = $formtype->name;
                                         @endphp
-
-                                        {{-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js">
-
-                                            function change($formname){
-                                                var element;
-                                                if (document.getElementById($formname){
-                                                    id = document.getElementById($formname).value;
-                                                    if($formname == "テキストボックス"){
-                                                    element = document.getElementById("item_content1");
-                                                    element = document.getElementById("item_content2");
-                                                    element = document.getElementById("item_content3");
-                                                    element.disabled = true;
-                                                    }else{
-                                                        element.disabled = false;
-                                                    }
-                                                }
-
-                                            }
-                                            console.log(onchange);
-                                        </script> --}}
                                     </td>
                                     <td>
                                         <input type="checkbox" name="must[]" value="必須" id="check-id" class="form-check-input">
@@ -61,18 +41,18 @@
                                     </td>
                                     <td>
                                         <div>
-                                            <input class="form-control" id="item_content1" placeholder="回答欄1"name="item_content1" type="text" disabled="disabled">
+                                            <input class="form-control item_content{{$i}}" id="item_content1-{{$i}}" placeholder="回答欄1"name="item_content1" type="text" disabled="disabled">
                                         </div>
                                         <div>
-                                            <input class="form-control" id="item_content2" placeholder="回答欄2"name="item_content2" type="text" disabled="disabled">
+                                            <input class="form-control item_content{{$i}}" id="item_content2-{{$i}}" placeholder="回答欄2"name="item_content2" type="text" disabled="disabled">
                                         </div>
                                         <div>
-                                            <input class="form-control" id="item_content3" placeholder="回答欄3"name="item_content3" type="text" disabled="disabled">
+                                            <input class="form-control item_content{{$i}}" id="item_content3-{{$i}}" placeholder="回答欄3"name="item_content3" type="text" disabled="disabled">
                                         </div>
                                         <div>
-                                            <input class="form-control" id="item_content4" placeholder="回答欄4"name="item_content4" type="text" disabled="disabled">
+                                            <input class="form-control item_content{{$i}}" id="item_content4-{{$i}}" placeholder="回答欄4"name="item_content4" type="text" disabled="disabled">
                                         </div>
-                                        <input class="form-control" id="item_content5" placeholder="回答欄5"name="item_content5" type="text" disabled="disabled">
+                                        <input class="form-control item_content{{$i}}" id="item_content5-{{$i}}" placeholder="回答欄5"name="item_content5" type="text" disabled="disabled">
                                     </td>
                                 </tr>
                             @endfor
